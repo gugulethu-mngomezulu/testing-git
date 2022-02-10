f79a167f2f00d8665b1371facb02fdfe4810e7a1
gugulethu-mngomezulu <nonthandogugulethu@gmail.com>
2022-02-11 00:29:41 +0200

    mission 1

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE city (
+
+	city_id int primary key,
+	name varchar(17),
+	countrycode varchar(3),
+	district varchar(20),
+	population int
+);
+
+insert into city values (1,'Los Angelos','USA','America',120000);
+select *from city
+
+insert into city values (4,'New York','USA','America',150000);
+select *from city
+
+select name
+  from city where population > 120000 and countrycode = 'USA';
